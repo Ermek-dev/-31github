d64994817bd3e4b9fbeae3e201746877b8ec0fe9
Severus <83870911+Ermek-dev@users.noreply.github.com>
2023-12-03 18:07:20 +0600

    Add files via upload

---
+++ task_20.py
@@ -0,0 +1,106 @@
+import random
+from typing import Dict, List
+
+
+# list_words = [
+#     {
+#         "name":"apple",
+#         "name":"orange",
+#         "name":"tomato"
+#     }
+# ]
+
+
+# random_list = random.choice(list_words)
+
+# user_input = input()
+# if user_input in random_list:
+#     print("Word found")
+# else:
+#     print("Word not found")    
+
+
+
+# user_input = input("Enter the word:")
+# for i in user_input:
+#     if i in random_list:
+#         print(i-random_list)
+# user_input = input("Enter the word:")
+# if user_input in random_list:
+#     print(user_input)
+# # print(random_list)
+
+# secret_word[i] = open_word[i]. 
+
+
+# def condition_game():
+
+
+
+
+# def amount_word():
+
+
+# fruits = [
+#     {
+#     "first_name": "Banana",
+#     "second_name": "Apple",
+#     "third_name": "Orange"
+# }
+# ]
+
+# def print_info_list_word(*args):
+#     print("%10s " % {args})
+
+
+# def get_list_word(fruits:Dict) ->List:
+#     print_info_list_word("Name")
+#     for fruit in fruits:
+#         print_info_list_word(fruit["first_name"],fruit["second_name"],fruit["third_name"]])
+
+
+# get_list_word()
+
+
+
+# import random
+
+# words = ["banana","apple","orange","strawberry","lemon"]
+
+# random_list = random.choice(words)
+
+# user_input = input("Enter the word:")
+
+# if user_input==random_list:
+#     print("Wow")        
+# else:
+#     print("Try again")    
+
+# print(words,random_list)
+
+
+# text = "Hello world"
+# search = input("Enter the world:").lower()
+# if search in text.lower():
+#     print(search)
+# else:
+#     print("Not found")    
+    
+import random   
+
+count = 0
+text = "Hello world"
+search = input("Enter the world:").lower()
+if search not in text.lower():
+    print(search)
+else:
+    print("Not found")     
+
+
+# print(text.startswith("H"))     
+# print(text.endswith("k"))  
+
+
+
+ 
+
